9fb54b2806fcc4725e51773675ff9cd21f805a3f
SriakashA <sriakash39@gmail.com>
2023-03-31 11:52:57 +0530

    login

--- employee/src/app/app-routing.module.ts
+++ employee/src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddContactComponent } from './component/add-contact/add-contact/add-contact.component';
-import { ContactManagerComponent } from './component/contact-manager/contact-manager/contact-manager.component';
-import { EditContactComponent } from './component/edit-contact/edit-contact/edit-contact.component';
-import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound/pagenotfound.component';
-import { ViewContactComponent } from './component/view-contact/view-contact/view-contact.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/contact-manager',pathMatch:'full'},
-  {path:'add-contact', component:AddContactComponent},
-  {path:'view-contact',component:ViewContactComponent},
-  {path:'edit-contact',component:EditContactComponent},
-  {path:'contact-manager',component:ContactManagerComponent},
-  {path:'**',component:PagenotfoundComponent},
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'login', component:LoginComponent},
 
 ];
 
--- employee/src/app/app.module.ts
+++ employee/src/app/app.module.ts
@@ -12,24 +12,14 @@ import {MatCardModule} from '@angular/material/card';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-import { AddContactComponent } from './component/add-contact/add-contact/add-contact.component';
-import { ContactManagerComponent } from './component/contact-manager/contact-manager/contact-manager.component';
-import { EditContactComponent } from './component/edit-contact/edit-contact/edit-contact.component';
-import { SpinnerComponent } from './component/spinner/spinner/spinner.component';
-import { ViewContactComponent } from './component/view-contact/view-contact/view-contact.component';
-import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimeCardComponent,
     EmployeeDetailsComponent,
-    AddContactComponent,
-    ContactManagerComponent,
-    EditContactComponent,
-    SpinnerComponent,
-    ViewContactComponent,
-    PagenotfoundComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
